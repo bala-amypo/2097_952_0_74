705409de06cc84b7a4f3f63def0e27ffada2e4dd
bala-amypo <bala@amypo.in>
2025-12-18 05:51:33 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -33,4 +33,7 @@ public class StudentController {
         return studentService.getStudentById(id);
     }
     @GetMapping("/update/{id}")
+    public String update(@PathVariable Long id,@RequestBody Student st){
+        return studentRepository.updateData(id,st);
+    }
 }
